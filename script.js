81985ad6acb51d0e4f982588e4612f266ffdd655
Jason Sutliff <jps@JSmbp.local>
2018-10-28 23:05:39 -0700

    event listener added to button NO JQUERY

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log('hello world')
+const submitButton = document.getElementById('submitButton');
+const companyName = document.getElementById('companyName');
+
+submitButton.addEventListener('click', (e) => {
+  e.preventDefault();
+  console.log(companyName.value)
+})
+
